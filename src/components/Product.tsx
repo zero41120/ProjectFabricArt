9ed29a1d6c6ad4e134ff8a841d8bc5fedbd9f9ec
zero41120 <devtlin34@gmail.com>
2022-08-08 17:33:26 -0700

    Add product spec

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,21 @@
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { Button, Card, CardActions, CardContent, CardMedia, Grid, TextField, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  TextField,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 import Swal from 'sweetalert2';
 import products from '../data/product';
@@ -49,11 +65,119 @@ export const Product = () => {
   return (
     <Grid container spacing={2} padding={2}>
       {products.map((product, index) => (
-        <Grid xs={12} sm={6} md={4} lg={3} item key={index}>
+        <Grid xs={12} md={6} item key={index}>
           <Card>
             <CardMedia sx={{ objectFit: 'contain' }} height={200} component="img" image={product.image} />
+            <Accordion square>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                <Typography>{product.name}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List>
+                  <ListItem>
+                    <Grid container>
+                      <Grid item xs={3}>
+                        <Typography>品名</Typography>
+                      </Grid>
+                      <Grid item xs={9}>
+                        <Typography>{product.name}</Typography>
+                      </Grid>
+                    </Grid>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem>
+                    <Grid container>
+                      <Grid item xs={3}>
+                        <Typography>內容量</Typography>
+                      </Grid>
+                      <Grid item xs={9}>
+                        <Typography>{product.spec.size}</Typography>
+                      </Grid>
+                    </Grid>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem>
+                    <Grid container>
+                      <Grid item xs={3}>
+                        <Typography>有效期限</Typography>
+                      </Grid>
+                      <Grid item xs={9}>
+                        <Typography>{product.spec.expireDate}</Typography>
+                      </Grid>
+                    </Grid>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem>
+                    <Grid container>
+                      <Grid item xs={3}>
+                        <Typography>原產地</Typography>
+                      </Grid>
+                      <Grid item xs={9}>
+                        <Typography>{product.spec.location}</Typography>
+                      </Grid>
+                    </Grid>
+                  </ListItem>
+                  <Divider />
+                  <ListItem>
+                    <Grid container>
+                      <Grid item xs={3}>
+                        <Typography>保存方式</Typography>
+                      </Grid>
+                      <Grid item xs={9}>
+                        {product.spec.storage.map((detail) => (
+                          <ul>
+                            <li>
+                              <Typography>{detail}</Typography>
+                            </li>
+                          </ul>
+                        ))}
+                      </Grid>
+                    </Grid>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem>
+                    <Grid container>
+                      <Grid item xs={3}>
+                        <Typography>使用方式</Typography>
+                      </Grid>
+                      <Grid item xs={9}>
+                        {product.spec.usage.map((detail) => (
+                          <ul>
+                            <li>
+                              <Typography>{detail}</Typography>
+                            </li>
+                          </ul>
+                        ))}
+                      </Grid>
+                    </Grid>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem>
+                    <Grid container>
+                      <Grid item xs={3}>
+                        <Typography>注意事項</Typography>
+                      </Grid>
+                      <Grid item xs={9}>
+                        {product.spec.note.map((detail) => (
+                          <ul>
+                            <li>
+                              <Typography>{detail}</Typography>
+                            </li>
+                          </ul>
+                        ))}
+                      </Grid>
+                    </Grid>
+                  </ListItem>
+                  <Divider />
+                </List>
+              </AccordionDetails>
+            </Accordion>
             <CardContent>
-              <Typography>{product.name}</Typography>
               <Typography color="secondary.main">{`單價 新台幣 ${product.price}元 (${product.unit})`}</Typography>
               <Typography variant="caption">{`批發價新台幣 ${product.bulkPrice}元 (${product.bulkAmount}${product.unit}以上)`}</Typography>
             </CardContent>
